0826837f93324b83f0fd9dff62f14e9477d014a6
Amund Fredriksen <amund.fredriksen@outlook.com>
2020-11-06 18:13:58 +0100

    Added plot_spinMatrix() to utils.py, which plots a binary colormap. -1 = black, +1 = white.

--- py_codes/utils.py
+++ py_codes/utils.py
@@ -1,6 +1,7 @@
 # This file contains useful functions to be used in other .py files.
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def energy_EV_analytical_2x2(T, J, kB):
     # Returns the analytical expectation value for the 2x2 case,
@@ -13,3 +14,28 @@ def energy_EV_analytical_2x2(T, J, kB):
     # The analytical expression:
     return -8*J*(np.sinh(x) / (np.cosh(x) + 3))
 
+'''
+def read_matrix_from_CSV(filename, directory):
+    directory = "../results/3g_three_body/"
+
+    # Read the planet names from the planet_names.csv file:
+    
+    #planetNamesData = np.loadtxt(directory + "planet_names.csv", skiprows=0, delimiter=",")
+    #planetNamesData = pd.DataFrame(planetNamesData, columns=["planet_names"])
+    #planetNames = planetNamesData["planet_names"]
+    
+    # Read in the file as a dataframe:
+    dataFrame = pd.read_csv(directory + filename, header=None)
+'''
+
+def plot_spinMatrix(spinMatrix):
+    # input: spinMatrix: A numpy array containing values -1 and 1.
+    # This function plots a colormap of black (-1) and white (+1) squares.
+
+    tempColorMatrix = spinMatrix
+    tempColorMatrix[spinMatrix==-1] = 0
+    plt.imshow(tempColorMatrix, cmap = 'gray') # The 'gray' colormap is binary:
+    # black for values below 1 and white for values >= 1.
+    plt.show()
+
+    

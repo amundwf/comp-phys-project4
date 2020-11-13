0e70bbabc15ee8c1b077ab58b2b958f8c76d5bdc
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-11-13 13:17:19 +0100

    Update utils.py

--- py_codes/utils.py
+++ py_codes/utils.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def energy_EV_analytical_2x2(T, J, kB):
     # Returns the analytical expectation value for the 2x2 case,
@@ -38,4 +39,25 @@ def plot_spinMatrix(spinMatrix):
     # black for values below 1 and white for values >= 1.
     plt.show()
 
+def plot_E_and_M():
     
+    directory = "../results/4c_ising/"
+    filename = "blabla_4c.csv"
+    
+    data = np.loadtxt(directory +filename, skiprows=1, delimiter=",")
+    data = pd.DataFrame(data, columns=["MC_cycles", "E", "M"])
+    
+    f = plt.figure(figsize=(18, 10), dpi=80, facecolor='w', edgecolor='k')
+    
+    plt.subplot(211)
+    plt.plot(data['MC_cycles'], data['E']/4, '.')
+    plt.xlabel("Monte Carlo cycles")
+    plt.ylabel("Expectation Energy")
+    plt.title("Expectation values for {} Monte Carlo cycles for a 2x2 spin matrix".format(len(data["MC_cycles"])))
+    
+    plt.subplot(212)
+    plt.plot(data['MC_cycles'], data['M']/4, '.')
+    plt.xlabel("Monte Carlo cycles")
+    plt.ylabel("Expectation Magnetisation")
+    
+plot_E_and_M()

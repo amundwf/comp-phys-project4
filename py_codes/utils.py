aa89786a149ac8d17ac967c36b072120054778ec
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-11-13 14:21:36 +0100

    Update utils.py

--- py_codes/utils.py
+++ py_codes/utils.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 12})
 import pandas as pd
 
 def energy_EV_analytical_2x2(T, J, kB):
@@ -45,19 +46,49 @@ def plot_E_and_M():
     filename = "blabla_4c.csv"
     
     data = np.loadtxt(directory +filename, skiprows=1, delimiter=",")
-    data = pd.DataFrame(data, columns=["MC_cycles", "E", "M"])
+    data = pd.DataFrame(data, columns=["MC_cycles", "E", "E2", "M", "M2", "Mabs"])
+    
+    T = 1.0
+    L2 = 2*2
+    MC = data["MC_cycles"]
+    N_MC = len(data['MC_cycles']) -1
+    E = data["E"]
+    E2 = data["E2"]
+    M = data["M"]
+    M2 = data["M2"]
+    Mabs = data["Mabs"]
+    
+    CV = (E2 - E*E)/T*T
+    chi = (M2 - M*M)/T
     
     f = plt.figure(figsize=(18, 10), dpi=80, facecolor='w', edgecolor='k')
     
-    plt.subplot(211)
-    plt.plot(data['MC_cycles'], data['E']/4, '.')
+    plt.subplot(221)
+    plt.plot(MC, E/L2, '.')
+    plt.xscale("log")
     plt.xlabel("Monte Carlo cycles")
     plt.ylabel("Expectation Energy")
-    plt.title("Expectation values for {} Monte Carlo cycles for a 2x2 spin matrix".format(len(data["MC_cycles"])))
     
-    plt.subplot(212)
-    plt.plot(data['MC_cycles'], data['M']/4, '.')
+    plt.subplot(222)
+    plt.plot(MC, Mabs/L2, '.')
+    plt.xscale("log")
     plt.xlabel("Monte Carlo cycles")
     plt.ylabel("Expectation Magnetisation")
+    
+    plt.subplot(223)
+    plt.plot(MC, CV , '.')
+    plt.xscale("log")
+    plt.xlabel("Monte Carlo cycles")
+    plt.ylabel("Specific Heat")
+    
+    plt.subplot(224)
+    plt.plot(MC, chi , '.')
+    plt.xscale("log")
+    plt.xlabel("Monte Carlo cycles")
+    plt.ylabel("Magnetic Susceptibility")
+    
+    f.suptitle("Expectation values for {} Monte Carlo cycles for a 2x2 spin matrix".format(N_MC))
+    
+    
     
 plot_E_and_M()
